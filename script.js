e0a71e42380c16824aaed464e44bf10882037fc7
Uriel <uriel.lira@smartsuite.imd.ufrn.br>
2024-06-07 13:57:45 -0300

    adicionar mascara de cpf

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-window.alert("carregou!");
+//Função para adicionar máscara ao cpf
+function handleChangeCPF(event) {
+  const newValue = event.target.value
+    .replace(/\D/g, "")
+    .replace(/(\d{9})(\d)/, "$1-$2")
+    .replace(/(\d{6})(\d)/, "$1.$2")
+    .replace(/(\d{3})(\d)/, "$1.$2");
+
+  event.target.value = newValue;
+}
+
+//Adicionar eventListeners aos campos necessários
+document.getElementById("inputCPF").addEventListener("input", handleChangeCPF);
